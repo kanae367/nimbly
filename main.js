623e32c1ac43d574c539e682923a83ed9776286f
”Kodoku” <kodokunawa@gmail.com>
2023-06-06 15:26:15 +0300

    Исправлен js код

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //burger
 const popup = document.querySelector('.popup');
 const burger = document.querySelector('.topbar__nav');
-burger.addEventListener('click', () => popup.classList.add('popup_active'));
+burger.addEventListener('click', () => popup.classList.add('popup_active'))
 
 popup.addEventListener('click', (e) => {
     if(!(e.target.classList.contains('close-popup-btn') || e.target.classList.contains('popup__link'))) return;
@@ -13,33 +13,34 @@ popup.addEventListener('click', (e) => {
 const insightSection = document.getElementById('insight');
 let feedbackId = 1;
 
-//creating buttons based on 'slides' amount 
-(function(){
+//creates btn elements based on slides amount
+(() => {
     const switchBtnsContainer = document.querySelector('.insight__switch-btns');
     for(let i = 0; i < data.length; i++){
         const btn = document.createElement('span');
         btn.classList.add('insight__button');
         switchBtnsContainer.append(btn);
+        
     }
 })()
 
-//creating nav buttons
+//handles clicks on swiper controls
 document.getElementById('switch-left').addEventListener('click', () => changeFeedback(--feedbackId));
 document.getElementById('switch-right').addEventListener('click', () => changeFeedback(++feedbackId));
 
 const feedback = {
-    _feedbackImage: insightSection.querySelector('.insight__image'),
-    _feedbackText: insightSection.querySelector('.insight__feedback-text'),
-    _feedbackUserName: insightSection.querySelector('.insight__user-name'),
-    _feedbackUserInfo: insightSection.querySelector('.insight__user-info'),
-    
+    feedbackImage: insightSection.querySelector('.insight__image'),
+    feedbackText: insightSection.querySelector('.insight__feedback-text'),
+    feedbackUserName: insightSection.querySelector('.insight__user-name'),
+    feedbackUserInfo: insightSection.querySelector('.insight__user-info'),
+
     setData: function(){
-        this._feedbackImage.src = data[feedbackId].img,
-        this._feedbackText.textContent = data[feedbackId].text,
-        this._feedbackUserName.textContent = data[feedbackId].name,
-        this._feedbackUserInfo.textContent = data[feedbackId].position
+        this.feedbackImage.src = data[feedbackId].img,
+        this.feedbackText.textContent = data[feedbackId].text,
+        this.feedbackUserName.textContent = data[feedbackId].name,
+        this.feedbackUserInfo.textContent = data[feedbackId].position
     },
-    
+
     changeData: function(){
         insightSection.style.opacity = 0;
         setTimeout(() => {
@@ -52,13 +53,13 @@ const feedback = {
 const btns = document.querySelectorAll('.insight__button');
 
 function switchActiveClass(){
-    btns.forEach(item => item.classList.remove('insight__button_active'));
+    btns.forEach(item=> item.classList.remove('insight__button_active'));
     btns[feedbackId].classList.add('insight__button_active');
 }
 
 function handleFeedbackIdChange(){
-    if(feedbackId < 0) feedbackId = data.length-1;
     if(feedbackId >= data.length) feedbackId = 0;
+    if(feedbackId < 0) feedbackId = data.length -1;
 }
 
 function changeFeedback(){
@@ -67,16 +68,15 @@ function changeFeedback(){
     switchActiveClass();
 }
 
-//setting initial feedback
+//sets initial feedback
 changeFeedback(feedbackId);
 
 //features swap
 
 const featuresElements = document.querySelectorAll('.features__item');
-featuresElements.forEach(item => item.addEventListener('click', () => changeActiveFeature(item)))
+featuresElements.forEach(item => item.addEventListener('click', () => handleFeaturesSwap(item)));
 
-
-function changeActiveFeature(currentFeature){
+function handleFeaturesSwap(currentFeature){
     featuresElements.forEach(item => item.classList.remove('features__item_active'))
     currentFeature.classList.add('features__item_active')
 }

5095f9604245d39580bc34868051d6d5b2b11393
”Kodoku” <kodokunawa@gmail.com>
2023-06-06 14:06:42 +0300

    исправлен js код

--- main.js
+++ main.js
@@ -1,91 +1,84 @@
 (() => {
-//бургер меню
+//burger
 const popup = document.querySelector('.popup');
 const burger = document.querySelector('.topbar__nav');
-burger.addEventListener('click', () => {
-    popup.classList.add('popup_active');
-})
+burger.addEventListener('click', () => popup.classList.add('popup_active'));
 
 popup.addEventListener('click', (e) => {
-    const target = e.target;
-    if(!(target.classList.contains('close-popup-btn') || target.classList.contains('popup__link'))) return;
-    
+    if(!(e.target.classList.contains('close-popup-btn') || e.target.classList.contains('popup__link'))) return;
     popup.classList.remove('popup_active');
 })
 
-//"Слайдер"
+//"Swiper"
 const insightSection = document.getElementById('insight');
-
-const feedback = {
-    feedbackImage: insightSection.querySelector('.insight__image'),
-    feedbackText: insightSection.querySelector('.insight__feedback-text'),
-    feedbackUserName: insightSection.querySelector('.insight__user-name'),
-    feedbackUserInfo: insightSection.querySelector('.insight__user-info'),
-
-    setData: function(){
-        this.feedbackImage.src = data[feedbackId].img,
-        this.feedbackText.textContent = data[feedbackId].text,
-        this.feedbackUserName.textContent = data[feedbackId].name,
-        this.feedbackUserInfo.textContent = data[feedbackId].position
-    }
-}
-
-const changeDataWithAnimation = () => {
-    insightSection.style.opacity = 0;
-
-    setTimeout(() => {
-        feedback.setData();
-        insightSection.style.opacity = 1;
-    }, 500)
-}
-
-const switchActiveBtnClass = () => {
-    document.querySelectorAll('.insight__button').forEach((item, index) => {
-        item.classList.remove('insight__button_active');
-        if(index === feedbackId){
-            item.classList.add('insight__button_active');
-        }
-    })
-}
-
-const changeFeedback = () => {
-    if(feedbackId >= data.length){
-        feedbackId = 0;
-    }else if(feedbackId < 0){
-        feedbackId = data.length -1;
-    }
-    changeDataWithAnimation(feedbackId);
-    switchActiveBtnClass(feedbackId);
-}
-
-
-const switchLeft = document.getElementById('switch-left');
-const switchRight = document.getElementById('switch-right');
 let feedbackId = 1;
 
-switchLeft.addEventListener('click', () => changeFeedback(--feedbackId));
-switchRight.addEventListener('click', () => changeFeedback(++feedbackId));
-const switchBtnsContainer = document.querySelector('.insight__switch-btns');
+//creating buttons based on 'slides' amount 
+(function(){
+    const switchBtnsContainer = document.querySelector('.insight__switch-btns');
+    for(let i = 0; i < data.length; i++){
+        const btn = document.createElement('span');
+        btn.classList.add('insight__button');
+        switchBtnsContainer.append(btn);
+    }
+})()
 
-const createBtn = () => {
-    const btn = document.createElement('span');
-    btn.classList.add('insight__button');
-    switchBtnsContainer.append(btn);
+//creating nav buttons
+document.getElementById('switch-left').addEventListener('click', () => changeFeedback(--feedbackId));
+document.getElementById('switch-right').addEventListener('click', () => changeFeedback(++feedbackId));
+
+const feedback = {
+    _feedbackImage: insightSection.querySelector('.insight__image'),
+    _feedbackText: insightSection.querySelector('.insight__feedback-text'),
+    _feedbackUserName: insightSection.querySelector('.insight__user-name'),
+    _feedbackUserInfo: insightSection.querySelector('.insight__user-info'),
+    
+    setData: function(){
+        this._feedbackImage.src = data[feedbackId].img,
+        this._feedbackText.textContent = data[feedbackId].text,
+        this._feedbackUserName.textContent = data[feedbackId].name,
+        this._feedbackUserInfo.textContent = data[feedbackId].position
+    },
+    
+    changeData: function(){
+        insightSection.style.opacity = 0;
+        setTimeout(() => {
+            this.setData();
+            insightSection.style.opacity = 1;
+        }, 500)
+    }
 }
 
-for(let i = 0; i < data.length; i++){
-    createBtn(i);
+const btns = document.querySelectorAll('.insight__button');
+
+function switchActiveClass(){
+    btns.forEach(item => item.classList.remove('insight__button_active'));
+    btns[feedbackId].classList.add('insight__button_active');
 }
 
+function handleFeedbackIdChange(){
+    if(feedbackId < 0) feedbackId = data.length-1;
+    if(feedbackId >= data.length) feedbackId = 0;
+}
+
+function changeFeedback(){
+    handleFeedbackIdChange();
+    feedback.changeData();
+    switchActiveClass();
+}
+
+//setting initial feedback
 changeFeedback(feedbackId);
 
+//features swap
 
 const featuresElements = document.querySelectorAll('.features__item');
-featuresElements.forEach(item => {
-    item.addEventListener('click', function(){
-        featuresElements.forEach(item => item.classList.remove('features__item_active'))
-        this.classList.add('features__item_active')
-    })
-})
+featuresElements.forEach(item => item.addEventListener('click', () => changeActiveFeature(item)))
+
+
+function changeActiveFeature(currentFeature){
+    featuresElements.forEach(item => item.classList.remove('features__item_active'))
+    currentFeature.classList.add('features__item_active')
+}
 
 })();
